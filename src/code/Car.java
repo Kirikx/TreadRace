1444e486d7ce75639db291bdb34e23314d89223a
kirikx <kirikx@mail.ru>
2019-12-03 22:48:46 +0300

    Применил защелку на старт CountDownLatch

--- src/code/Car.java
+++ src/code/Car.java
@@ -1,7 +1,10 @@
 package code;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private CountDownLatch start;
     static {
         CARS_COUNT = 0;
     }
@@ -14,7 +17,8 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch start) {
+        this.start = start;
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
@@ -26,6 +30,8 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            start.countDown();
+            start.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
